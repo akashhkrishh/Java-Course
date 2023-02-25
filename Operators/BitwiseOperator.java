2ced0b236e9a446249300396b9d1482abd785439
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-25 11:31:37 +0530

    Create BitwiseOperator.java

---
+++ Operators/BitwiseOperator.java
@@ -0,0 +1,17 @@
+class BitwiseOperator{
+    public static void main(String arg[]){
+        
+        System.out.println("\nBitwise Operators :");
+        int a = 10;
+        int b = 20;
+        int c ;
+        System.out.println("\nA = "+a+" B = "+b);
+        c = a^b ; 
+        System.out.println("\nA^B : "+c);
+        c = a&b ; 
+        System.out.println("\nA&B : "+c);
+        c = a|b ; 
+        System.out.println("\nA|B : "+c);
+        
+    }
+}
