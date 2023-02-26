b810b6304a1bd7980123801cf6f29e2ad8ae9cca
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-26 10:00:14 +0530

    Create ForLoop.java

---
+++ Looping Statement/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop{
+    public static void main(String arg[]){
+        System.out.println("For Loop : ");
+        
+        int num=100;
+        for(int i=1;i<=num;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
