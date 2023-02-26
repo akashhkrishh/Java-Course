7e0bd449aafd49f0720e57a3e81390a22fa7c420
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-26 10:02:02 +0530

    Create WhileLoop.java

---
+++ Looping Statement/WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop{
+    public static void main(String arg[]){
+        System.out.println("While Loop : ");
+        
+        int num=100;
+        int i=1;
+        while(i<=num){
+            System.out.print(i+" ");
+            i+=1;
+        }
+    }
+}
