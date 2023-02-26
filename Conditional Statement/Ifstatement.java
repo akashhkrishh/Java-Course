8224b7f7e075f5cdc2d8405e602f515e78f22ab3
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-26 09:34:50 +0530

    Add files via upload

---
+++ Conditional Statement/Ifstatement.java
@@ -0,0 +1,13 @@
+public class Ifstatement{
+    public static void main(String arg[]){
+        System.out.println("If Statement : ");
+        int x=100;
+        if(x>10){
+            System.out.println("The \'X\' is Greater Than 10 ");
+        }
+        else{
+            System.out.println("The \'X\' is Less Than 10 ");
+        }
+
+    }
+}
