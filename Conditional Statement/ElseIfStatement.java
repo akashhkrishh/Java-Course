83574063d3a01f71936a1af76df556e5e7b32f47
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-26 09:46:09 +0530

    Create ElseIfStatement.java

---
+++ Conditional Statement/ElseIfStatement.java
@@ -0,0 +1,16 @@
+public class ElseIfStatement{
+    public static void main(String arg[]){
+        System.out.println("Else-If Statement : ");
+        int age=18;
+        if(age>18){
+            System.out.println("Age is Greater than 18");
+        }
+        else if(age==18){
+            System.out.println("Age is Equal to 18");
+        }
+        else{
+           System.out.println("Age is Less than 18"); 
+        }
+
+    }
+}
