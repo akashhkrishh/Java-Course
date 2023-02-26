03a1c1007292c7f3796515cc85ddba6e291c54c6
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-26 10:05:31 +0530

    Create DoWhileLoop.java

---
+++ Looping Statement/DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop{
+    public static void main(String arg[]){
+        System.out.println("While Loop : ");
+        
+        int num=100;
+        int i=1;
+        do {
+            System.out.print(i+" ");
+            i+=1;
+        }while(i<=num);
+    }
+}
