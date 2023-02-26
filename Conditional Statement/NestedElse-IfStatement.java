0b6f1141cd0e44a5912667b1f8f7ebf6ab8339d3
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-26 09:42:15 +0530

    Create NestedElse-IfStatement.java

---
+++ Conditional Statement/NestedElse-IfStatement.java
@@ -0,0 +1,23 @@
+public class NestedElse-IfStatement{
+    public static void main(String arg[]){
+        System.out.println("Nested else-If Statement : ");
+        int x=1010;
+        int y=4599;
+        int z=123;
+        if(x>y){
+            if(x>z){
+                System.out.println("Greater Value is : "+x);    
+            }
+            else{
+                System.out.println("Greater Value is : "+z);
+            }
+        }
+        else if(y>z){
+            System.out.println("Greater Value is : "+y);
+        }
+        else{
+           System.out.println("Greater Value is : "+z); 
+        }
+
+    }
+}
