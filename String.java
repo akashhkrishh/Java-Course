c60175581df500d961476291727c58320fade603
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-03-02 11:42:24 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,11 @@
+class String{
+	public static void main(String[] args) {
+		String name1 = "Antony";
+		String name2 = new String("Shelkton");
+		System.out.println(name1);
+		System.out.println(name2);
+		System.out.println(name1.concat(" "+name2));
+		System.out.println(name1.charAt(0));
+		System.out.println(name1+name2);		
+	}
+}
